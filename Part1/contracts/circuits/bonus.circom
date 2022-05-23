4ec806b8f41bb62944c4a00648a59f1e085b5f7a
Joao Cyrino <joaopedrocancado@gmail.com>
2022-05-23 14:43:20 -0300

    week 3 zku

--- Part1/contracts/circuits/bonus.circom
+++ Part1/contracts/circuits/bonus.circom
@@ -1,1 +1,91 @@
-// [bonus] implement an example game from part d
+pragma circom 2.0.0;
+
+include "../../node_modules/circomlib/circuits/poseidon.circom";
+include "../../node_modules/circomlib/circuits/comparators.circom";
+include "../../node_modules/circomlib/circuits/mux1.circom";
+
+template MerkleTreeNoInclusionProof(n) {
+    var numLeaves = 2 ** n;
+
+    // The round secret that can not be in the merkle tree
+    signal input secret;
+
+    // All the leaves of the merkle tree
+    signal input leaves[numLeaves];
+
+    // The calculated root of the merkle tree
+    signal output root;
+
+    component hashes[numLeaves - 1];
+
+    var numFirstHashes = numLeaves / 2;
+    var i;
+
+    for (i=0; i < numFirstHashes; i++) {
+        // Checks if the secret is in the merkle tree
+        assert(secret != leaves[i * 2]);
+        assert(secret != leaves[i * 2 + 1]);
+
+        // calculates the hashes of the leaves in pair
+        hashes[i] = Poseidon(2);
+        hashes[i].inputs[0] <== leaves[i * 2];
+        hashes[i].inputs[1] <== leaves[i * 2 + 1];
+    }
+
+    var k = 0;
+    for (i=numFirstHashes; i < numLeaves - 1; i++) {
+        // calculates the hashes based on previous hashes of merkle tree
+        hashes[i] = Poseidon(2);
+        hashes[i].inputs[0] <== hashes[k * 2].out;
+        hashes[i].inputs[1] <== hashes[k * 2 + 1].out;
+        k++;
+    }
+
+    root <== hashes[numLeaves - 2].out;
+}
+
+template OddsAndEvens() {
+    // all leaves of the merkle tree
+    signal input leaves[8];
+    // the merkle tree root on the blockchain
+    signal input root;
+
+    // the number that the first player has chosen
+    signal input player_one_num;
+    // the round secret tha the first player provided
+    signal input player_one_round_secret;
+
+    // the number that the second player has chosen
+    signal input player_two_num;
+    // the round secret tha the second player provided
+    signal input player_two_round_secret;
+
+    // the output that represent if even or odd wins => 0 for odd and 1 for even
+    signal output is_even;
+
+    // checks if both secret provided are the same (if it is the same round)
+    player_one_round_secret === player_two_round_secret;
+
+    component noInclusionProof = MerkleTreeNoInclusionProof(3);
+
+    // passes the secret to check if exists on the merkle tree
+    noInclusionProof.secret <== player_one_round_secret;
+
+    for (var i = 0; i < 8; i++) {
+        noInclusionProof.leaves[i] <== leaves[i];
+    }
+
+    // checks if the root on the blockchain is the same than the calculated
+    root === noInclusionProof.root;
+
+    var sum = player_one_num + player_two_num;
+    var remainder = sum % 2;
+
+    component isZero = IsZero();
+
+    isZero.in <-- remainder;
+
+    is_even <== isZero.out;
+}
+
+component main = OddsAndEvens();
--- Part1/test/bonus-test.js
+++ Part1/test/bonus-test.js
@@ -1,1 +1,100 @@
-// [bonus] unit test for bonus.circom
+const chai = require("chai");
+
+const wasm_tester = require("circom_tester").wasm;
+const buildPoseidon = require("circomlibjs").buildPoseidon;
+
+const F1Field = require("ffjavascript").F1Field;
+const Scalar = require("ffjavascript").Scalar;
+exports.p = Scalar.fromString("21888242871839275222246405745257275088548364400416034343698204186575808495617");
+const Fr = new F1Field(exports.p);
+
+const assert = chai.assert;
+
+describe('Bonus test', () => {
+    let circuit, root, F, poseidon;
+    const leaves = [1, 0, 0, 0, 0, 0, 0, 0];
+
+    before(async function () {
+        try {
+            poseidon = await buildPoseidon();
+            F = poseidon.F;
+
+            const hashes = [];
+
+            let i;
+
+            for (i = 0; i < 4; i++) {
+                hashes.push(F.toObject(poseidon([leaves[i * 2], leaves[i * 2 + 1]])))
+            }
+
+            for (i = 0; i < 3; i++) {
+                hashes.push(F.toObject(poseidon([hashes[i * 2], hashes[i * 2 + 1]])))
+            }
+
+            root = hashes[6];
+
+            circuit = await wasm_tester("contracts/circuits/bonus.circom");
+            await circuit.loadConstraints();
+        } catch (e) {
+            console.log('befor error: ', e);
+        }
+    })
+
+    it("Should pass with valid values: odd wins", async () => {
+        const INPUT = {
+            leaves,
+            root,
+            player_one_num: 2,
+            player_one_round_secret: 2,
+            player_two_num: 3,
+            player_two_round_secret: 2,
+        }
+
+        const witness = await circuit.calculateWitness(INPUT, true);
+
+        assert(Fr.eq(Fr.e(witness[0]), Fr.e(1)));
+        assert(Fr.eq(Fr.e(witness[1]), Fr.e(0)));
+    });
+
+    it("Should pass with valid values: even wins", async () => {
+        const INPUT = {
+            leaves,
+            root,
+            player_one_num: 3,
+            player_one_round_secret: 2,
+            player_two_num: 3,
+            player_two_round_secret: 2,
+        }
+
+        const witness = await circuit.calculateWitness(INPUT, true);
+
+        assert(Fr.eq(Fr.e(witness[0]), Fr.e(1)));
+        assert(Fr.eq(Fr.e(witness[1]), Fr.e(1)));
+    });
+
+    it("Should throw an error on line 20 or 21 when round secret is on merkle tree", async () => {
+        try {
+            let r = F.toObject(poseidon([1, 1]))
+            r = F.toObject(poseidon([r, 0]))
+            r = F.toObject(poseidon([r, 0]))
+
+
+            const INPUT = {
+                leaves,
+                root,
+                player_one_num: 2,
+                player_one_round_secret: 1,
+                player_two_num: 3,
+                player_two_round_secret: 1,
+            }
+
+            await circuit.calculateWitness(INPUT, true);
+        } catch (e) {
+            const errString = e.toString();
+            const errLine = errString.indexOf('line: ');
+            const line = Number((errString.substr(errLine)).replace(/\D/g, ""));
+
+            assert(Fr.eq(Fr.e(line), Fr.e(20)) || Fr.eq(Fr.e(line), Fr.e(21)));
+        }
+    });
+});
